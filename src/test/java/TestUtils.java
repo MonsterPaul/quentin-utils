f17d4af99f798a8799f2e36ca980de4b2e7c2e63
quentin <qundongni@qq.com>
2018-01-04 11:34:40 +0800

    webservice客户端

--- src/test/java/TestUtils.java
+++ src/test/java/TestUtils.java
@@ -1,8 +1,9 @@
-import com.demo.quentin.utils.HCB;
 import com.demo.quentin.utils.HttpClientUtils;
-import com.demo.quentin.utils.HttpConfig;
-import org.apache.http.client.HttpClient;
+import com.demo.quentin.utils.ServiceBeanMessage;
+import com.demo.quentin.utils.WebServiceClientUtils;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,39 +15,42 @@ import java.util.Map;
  */
 public class TestUtils {
 
+    private static Logger logger = LoggerFactory.getLogger(TestUtils.class);
+
     /**
      * 测试糊涂工具类
      */
     @Test
     public void testHttpClient() {
         String url = "http://localhost:8080/ane-job/100219.bizservice";
-        //插件式配置生成HttpClient时所需参数（超时、连接池、ssl、重试）
-        HCB hcb = HCB.getInstance()
-                .timeOutConfig(3000, false)//超时
-                .retry(3);                   //重试3次
-
-        HttpClient client = hcb.build();
-
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("key1", "value1");
-        map.put("key2", "value2");
-
-        //插件式配置请求参数（网址、请求参数、编码、client）
-        HttpConfig config = HttpConfig.getInstance()
-                .urls(url)                    //设置请求的url
-                .map(map)            //设置请求参数，没有则无需设置
-                .encoding("utf-8") //设置请求和返回编码，默认就是Charset.defaultCharset()
-                .client(client)                                                        //如果只是简单使用，无需设置，会自动获取默认的一个client对象
-                .inenc("utf-8")                                                    //设置请求编码，如果请求返回一直，不需要再单独设置
-                .inenc("utf-8")                                                    //设置返回编码，如果请求返回一直，不需要再单独设置
-                //.json("json字符串")												//json方式请求的话，就不用设置map方法，当然二者可以共用。
-                //.context(HttpCookies.custom().getContext()) 		//设置cookie，用于完成携带cookie的操作
-                //.out(new FileOutputStream("保存地址"))			 	//下载的话，设置这个方法,否则不要设置
-                //.files(new String[]{"d:/1.txt","d:/2.txt"})					//上传的话，传递文件路径，一般还需map配置，设置服务器保存路径
-                ;
-
-        String result2 = HttpClientUtils.getInstance().post(config);    //post请求
-        System.out.println("返回结果："+result2);
-
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("ewbNo", "433122");
+        params.put("siteId", "4545");
+        HttpClientUtils.getInstance().sendPostReq(url, params, "utf-8");
     }
+
+    @Test
+    public void cxfWs() {
+        String url = "http://localhost:8080/services/NE8Service?wsdl";
+        String nameSpace = "http://web.operation.ne.chengfeng.com/";
+        String bindingNameStr = "NE8ServiceServiceSoapBinding";
+        String servicename = "NE8ServiceService";
+        String method = "callNE8LogicToString";
+        String str = "[{\"waybilllist\":\"[{\"ewbNo\": \"5712000\",\"siteName\": \"萧山分拨中心\"},{\"siteCode\": \"5392001\",\"siteName\": \"临沂分拨中心\"}]\",\"posVoucherNo\":\"1111111\",\"terminalNo\":\"212121\"}]";
+        byte[] k = {1, 1, 1, 1, 1, 1};
+        ServiceBeanMessage serviceBeanMessage = new ServiceBeanMessage();
+        serviceBeanMessage.setLoginUser("admin");
+        serviceBeanMessage.setLoginPwd("admin");
+        serviceBeanMessage.setIp("192.168.1.8");
+        serviceBeanMessage.setServerCode("200101");
+        serviceBeanMessage.setParams(str);
+        serviceBeanMessage.setScanParams(k);
+        serviceBeanMessage.setStates("1");
+
+
+        //调用WebService
+        WebServiceClientUtils.executeService(url, nameSpace, bindingNameStr, method, serviceBeanMessage);
+    }
+
+
 }
